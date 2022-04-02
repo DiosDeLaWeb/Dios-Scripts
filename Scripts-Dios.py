e4daa79cc27d91f9ffa982814d37c084b38d1ab4
DiosDeLaWeb <102493959+DiosDeLaWeb@users.noreply.github.com>
2022-04-01 23:53:16 -0300

    Add files via upload

---
+++ Scripts-Dios.py
@@ -0,0 +1,141 @@
+import os
+import sys
+import time
+
+#time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+#cores
+Azul = '\033[94m'
+Verde = '\033[92m'
+Amarelo = '\033[93m'
+Vermelho = '\033[91m'
+Fim = '\033[0m'
+
+#logo
+print ('''\033[93m───────▄█──────────█─────────█▄───────
+─────▐██──────▄█──███──█▄─────██▌─────
+────▐██▀─────█████████████────▀██▌────
+───▐██▌─────██████████████─────▐██▌───
+───████────████████████████────████───
+──▐█████──██████████████████──█████▌──
+───████████████████████████████████───
+────███████▀▀████████████▀▀███████────
+─────█████▌──▄▄─▀████▀─▄▄──▐█████─────
+───▄▄██████▄─▀▀──████──▀▀─▄██████▄▄───
+──██████████████████████████████████──
+─████████████████████████████████████─
+▐██████──███████▀▄██▄▀███████──██████▌
+▐█████────██████████████████────█████▌
+▐█████─────██████▀──▀██████─────█████▌
+─█████▄─────███────────███─────▄█████─
+──██████─────█──────────█─────██████──
+────█████────────────────────█████────
+─────█████──────────────────█████─────
+──────█████────────────────█████──────
+───────████───▄────────▄───████───────
+────────████─██────────██─████────────
+────────████████─▄██▄─████████────────
+───────████████████████████████───────
+───────████████████████████████───────
+────────▀█████████▀▀█████████▀────────
+──────────▀███▀────────▀███▀──────────\033[0m"
+\033[91m ____   ____ ____  ___ ____ _____ ____
+/ ___| / ___|  _ \|_ _|  _ \_   _/ ___|   
+\___ \| |   | |_) || || |_) || | \___ \
+ ___) | |___|  _ < | ||  __/ | |  ___) |
+|____/ \____|_| \_\___|_|    |_| |____/
+ ____ ___ ___  _______________________
+|  _ \_ _/ _ \/ ___|___________________
+| | | | | | | \___ \___________________
+| |_| | | |_| |___) |__________________
+|____/___\___/|____/___________________\033[0m
+
+\033[92mCODER BY: Dios De La Web
+NEW TEAM 404
+
+PREPARANDO VOCE PARA O HACKING\033[0m''')
+
+print "[+] \033[92mPREPARANDO...\033[0m "
+time.sleep(2)
+print "[+] \033[92mAGUARDE...\033[0m "
+time.sleep(2)
+print "[+] \033[92mCOMECANDO\033[0m "
+time.sleep(2)
+print "[+] \033[92mINSTALANDO FERRAMENTAS\033[0m "
+
+#figlet
+os.system("figlet FIGLET")
+os.system("apt install python")
+
+#sqlmap
+os.system("figlet SQLMAP")
+os.system("git clone https://github.com/sqlmapproject/sqlmap")
+
+#redhawk
+os.system("figlet REDHAWK")
+os.system("git clone https://github.com/RedhawkSDR/redhawk")
+
+#diosDDos
+os.system("figlet DIOS DDOS")
+os.system("git clone https://github.com/DiosDeLaWeb/Dios-DDos")
+
+#brute
+os.system("figlet BRUTE FORCE")
+os.system("git clone https://github.com/IAmBlackHacker/Facebook-BruteForce")
+
+#hydra
+os.system("figlet HYDRA")
+os.system("git clone https://github.com/Gameye98/Black-Hydra")
+
+#ngrok
+os.system("figlet NGROK")
+os.system("git clone https://github.com/bubenshchykov/ngrok")
+
+#camhack
+os.system("figlet CAM HACK")
+os.system("git clone https://github.com/AngelSecurityTeam/Cam-Hackers")
+
+#quacksms
+os.system("figlet QUACK SMS")
+os.system("git clone https://github.com/Monkey-hk4/quack")
+
+#hammer
+os.system("figlet HAMMER DDOS")
+os.system("git clone https://github.com/cyweb/hammer")
+
+#toolx
+os.system("figlet TOOL X")
+os.system("git clone https://github.com/rajkumardusad/Tool-X")
+
+#lazymux
+os.system("figlet LAZYMUX")
+os.system("git clone https://github.com/Gameye98/Lazymux")
+
+#brutal
+os.system("figlet BRUTAL")
+os.system("git clone http://github.com/Screetsec/Brutal")
+
+#torshammer
+os.system("figlet TORSHAMMER")
+os.system("git clone https://github.com/dotfighter/torshammer")
+
+#ipgeo
+os.system("figlet IPGEO")
+os.system("git clone https://github.com/maldevel/IPGeolocation")
+
+#wifihacker
+os.system("figlet WIFI HACKER")
+os.system("git clone https://github.com/esc0rtd3w/wifi-hacker")
+
+os.system("clear")
+
+#Fim
+os.system("figlet ADEUS")
+os.system("exit")
